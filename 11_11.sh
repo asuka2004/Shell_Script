37def7528fbe60cd5460cc14875031f28b14e381
root <Kung>
2021-03-30 10:18:23 +0800

    03/30-10:18

---
+++ 11_11.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PATH=/usr/local/mysql/bin/mysql
+DBPATH=/root/test
+MYUSER=root
+MYPASS=Ndijn7p9
+SOCKET=/usr/local/mysql/mysql.sock
+MYCMD="mysql -u$MYUSER -p$MYPASS -S $SOCKET"
+MYDUMP="mysqldump -u$MYUSER -p$MYPASS -S $SOCKET"
+
+[ ! -d "$DBPATH" ] && mkdir $DBPATH
+
+for dbname in `$MYCMD -e "show databases;"|sed '1,2d'`
+
+do
+ $MYDUMP $dbname| gzip >$DBPATH/${dbname}_$(date +%F).sql.gz
+done
+
