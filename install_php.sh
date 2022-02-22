08363a0e19b84c16af7f29d3228af1d6b5ec0b38
Kung <asuka.2004@gmail.com>
2022-02-22 22:29:13 +0800

    2022/02/22

---
+++ install_php.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+path=/root/gittest
+[ ! -d "$path" ]&& mkdir -p $path 
+
+cat<<END
+
+ 1.[install lamp]
+ 2.[install lnmp]
+ 3.[exit]
+Please input the num you want:
+END
+
+read num
+expr $num + 1 &>/dev/null
+[ $? -ne 0 ]&&{
+	echo "the num you input must be {1|2|3}"
+	exit 1
+}
+
+[ $num -eq 1 ]&&{
+	echo "Start installing lamp"
+	sleep 2;
+	
+	[ -x "$path/lamp.sh" ]||{
+	echo "$path/lamp.sh does not exist or can not be exec"
+	exit 1
+	}
+	$path/lamp.sh
+	exit $?
+}
+
+
+[ $num -eq 2 ]&&{
+	echo "Starting install lnmp"
+	sleep 2;
+
+	[ -x "$path/lnmp.sh"]||{
+	echo "$path/lnmp.sh does not exist or can not be exec"
+	exit 1
+	}
+	$path/lnmp.sh
+	exit $?
+}
+
+[ $num -eq 3 ]&&{
+	echo bye
+	exit 3
+
+}
+
+
+
+
+
