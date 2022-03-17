f711565ebbe3a0ded75a8616f7ec06cf553f1edb
Kung <asuka.2004@gmail.com>
2022-03-17 22:07:47 +0800

    2022/03/17

---
+++ array_kind.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+array=(1 2 3 4 5)
+
+for((i=0;i<${#array[*]};i++))
+do
+	echo ${array[i]}
+done
+
+
+for n in ${array[*]}
+do
+	echo $n
+done
+
+
+i=0
+while ((i<${#array[*]}))
+do
+	echo ${array[i]}
+	((i++))
+done
