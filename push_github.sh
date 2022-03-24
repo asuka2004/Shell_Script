75991689b6dbf228462bbf5715107f9ff9573ced
Kung <asuka.2004@gmail.com>
2022-03-24 23:58:44 +0800

    2022/03/24

--- push_github.sh
+++ push_github.sh
@@ -8,6 +8,6 @@
 # *************************************************
 export PATH=$PATH
 . /etc/init.d/functions
-	git add * 
+	git add --all 
 	git commit -m $(date +%Y/%m/%d)
 	git push -u origin master
