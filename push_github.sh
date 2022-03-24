1ab35e6fb66b6a0dd47960b25674685169a00109
Kung <asuka.2004@gmail.com>
2022-03-24 23:09:05 +0800

    2022/03/24

--- push_github.sh
+++ push_github.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
-#Author: Kung
-#Subject: Auto Git	
+#!/bin/sh
+# **************************************************
+# Description  : Push script to Github 
+# Build        : 2022-03-24 23:08
+# Author       : Kung
+# System       : 
+# Version      : 
+#              :     
+# *************************************************
+export PATH=$PATH
+. /etc/init.d/functions
 	git add * 
 	git commit -m $(date +%Y/%m/%d)
 	git push -u origin master
