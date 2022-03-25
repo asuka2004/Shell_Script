33eb7b47b262c1d8a439a88b3e59a69ba8fb24a2
Kung <asuka.2004@gmail.com>
2022-03-25 22:22:28 +0800

    2022/03/25

--- 19_1.sh
+++ 19_1.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+#!/bin/sh
+# **************************************************
+# Description  : Random output html
+# Build        : 2022-03-25 21:42
+# Author       : Kung
+# System       : CentOS 7.6
+# Version      : 1.1
+#              :     
+# *************************************************
+export PATH=$PATH
+. /etc/init.d/functions
+
 Path=/root/test
 [ -d "$PATH" ]|| mkdir -p $Path
 
@@ -7,5 +18,4 @@ do
 	random=$(openssl rand -base64 40| sed 's#[^a-z]##g'|cut -c 1-10)
 	touch $Path/${random}_.html
 done
-#author:
  
---
+++ lab_break.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# **************************************************
+# Description  : study "break continue exit return" 
+# Build        : 2022-03-25 21:43
+# Author       : Kung
+# System       : CentOS 7.6
+# Version      : 1.1
+#              :     
+# *************************************************
+export PATH=$PATH
+. /etc/init.d/functions
+
+if [ $# -ne 1 ];then
+	echo "Please input {break|continue|exit|return}"
+	exit 1
+fi
+
+test(){
+ for ((i=0;i<=5;i++))
+ do
+	if [ $i -eq 3 ];then
+		$*	
+	fi
+	echo $i	
+ done
+ echo " in func"
+}
+test $*
+ 
