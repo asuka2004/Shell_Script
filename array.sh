f6e0283f10747169382847b95df8e3fefc6b5bb3
Kung <asuka.2004@gmail.com>
2022-04-10 23:59:06 +0800

    2022/04/10

--- array.sh
+++ array.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # **************************************************
-# Description  : study array
+# Description  : char less than 4 
 # Build        : 2022-03-25 22:52
 # Author       : Kung
 # System       : CentOS 7.6
@@ -10,10 +10,10 @@
 export PATH=$PATH
 . /etc/init.d/functions
 
-array=( i am gilson not tony kung )
+array=(  gilson not tony kung  i m )
 for ((i=0;i<${#array[*]};i++))
 do
- 	if [ ${#array[$i]} -lt 3 ]
+ 	if [ ${#array[$i]} -lt 4 ]
 	 then
 		echo "${array[$i]}"
  	fi
