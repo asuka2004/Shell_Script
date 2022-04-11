45978ee5c844ca2c66ddf417332152df936374a2
Kung <asuka.2004@gmail.com>
2022-04-11 22:36:38 +0800

    2022/04/11

--- push_github.sh
+++ push_github.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 # **************************************************
-# Description  : Push script to Github 
-# Build        : 2022-03-24 23:08
+# Description  : 
+# Build        : 2022-04-11 22:36
 # Author       : Kung
-# System       : CentOS 7.6 
-# Version      : 1.1 
-# *************************************************
+# System       : 
+# Version      : 
+# ************************************************ 
+export LANG=C
 export PATH=$PATH
 . /etc/init.d/functions
+script_path=/root/github
+log_path=/root/tmp
 	git add --all 
 	git commit -m $(date +%Y/%m/%d)
 	git push -u origin master
