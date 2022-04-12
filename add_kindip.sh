7be12da7dfa4d92240effb28b643ecb548491a0b
Kung <asuka.2004@gmail.com>
2022-04-12 22:02:03 +0800

    2022/04/12

--- add_kindip.sh
+++ add_kindip.sh
@@ -1,53 +1,59 @@
 #!/bin/bash
 # **************************************************
-# Description  : add/del network ip address 
-# Build        : 2022-04-11 22:24
+# Description  : Batch add/del Network IP Address 
+# Build        : 2022-04-12 21:28
 # Author       : Kung
 # System       : CentOS 7.6
 # Version      : 1.1
 # ************************************************ 
+export PS4='+${LINENO} '
 export LANG=C
 export PATH=$PATH
-. /etc/init.d/functions
-script_path=/root/github
-log_path=/root/tmp
+[ -f /etc/init.d/functions ] && . /etc/init.d/functions
 
-[ -f /etc/init.d/functions ]&& . /etc/init.d/functions
+script_path=/root/github
+[ ! -d ${script_path} ] && mkdir -p ${script_path}
+log_path=/root/tmp
+[ ! -d ${log_path} ] && mkdir -p ${log_path}
+
 RETVAL=0
 add(){
-	for ip in {1..16}
+	for ip in {1..5}
 	do
-		if [ $ip -eq 10 ];then
+		if [ $ip -eq 10 ]
+		 then
 		continue
 		fi
 		
-		ip addr add 192.168.88.$ip/24 dev eth0 label eth0:$ip &>/dev/null
-		RETVAL=$?
+	ip addr add 192.168.88.$ip/24 dev eth0 label eth0:$ip &>/dev/null
+	RETVAL=$?
 
-		if [ $RETVAL -eq 0 ];then
-			action "add $ip" /bin/true
+		if [ $RETVAL -eq 0 ]
+		 then
+			action "Success to add 192.168.88.$ip" /bin/true
 		else
-			action "add $ip" /bin/false
+			action "Fail to add 192.168.88.$ip" /bin/false
 		fi
 	done
 	return $RETVAL
 }
 
-
 del(){
-	for ip in {16..1}
+	for ip in {5..1}
 	do
-		if [ $ip -eq 10 ];then
+		if [ $ip -eq 10 ]
+		 then
 			continue
 		fi
 
-		ifconfig eth0:$ip down &>/dev/null
-		RETVAL=$?
+	ifconfig eth0:$ip down &>/dev/null
+	RETVAL=$?
 
-		if [ $RETVAL -eq 0 ];then
-			action "del $ip" /bin/true
+		if [ $RETVAL -eq 0 ]
+		 then
+			action "Success to del 192.168.88.$ip" /bin/true
 		else
-			action "del $ip" /bin/false
+			action "Fail to del 192.168.88.$ip" /bin/false
 		fi
 	done
 }
