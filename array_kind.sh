48bd89f9a40b8d19c9c024d1cb2909072b2a07d8
Kung <asuka.2004@gmail.com>
2022-04-13 10:48:28 +0800

    2022-04-13-10:48

--- array_kind.sh
+++ array_kind.sh
@@ -1,14 +1,18 @@
-#!/bin/sh
-# **************************************************
-# Description  : Array study. Use kind of judgement    
-# Build        : 2022-03-24 23:46
-# Author       : Kung
-# System       : CentOS 7.6
-# Version      : 1.1
-#              :     
-# *************************************************
+#!/bin/bash
+# Author      : Kung
+# Build       : 2022-04-13 10:45
+# Version     : V1.0
+# Description : Array study. Use kind of judgement           
+# System      : CentOS 7.6 
+			       
+export PS4='++ ${LINENO}'  
+export LANG=C
 export PATH=$PATH
-. /etc/init.d/functions
+[ -f /etc/init.d/functions ] && . /etc/init.d/functions
+Script_Path=/root/github
+[ ! -d ${Script_Path} ] && mkdir -p ${Script_Path}
+Log_Path=/root/tmp
+[ ! -d ${Log_Path} ] && mkdir -p ${Log_Path}
 
 array=(1 2 3 4 5)
 
@@ -17,12 +21,10 @@ do
 	echo ${array[i]}
 done
 
-
 for n in ${array[*]}
 do
 	echo $n
 done
-
 
 m=0
 while ((m<${#array[*]}))
