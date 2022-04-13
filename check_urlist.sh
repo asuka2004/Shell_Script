7d8308650b389514422094ee60fda4e7cf28c188
Kung <asuka.2004@gmail.com>
2022-04-13 20:12:35 +0800

    2022-04-13-20:12

--- check_urlist.sh
+++ check_urlist.sh
@@ -1,42 +1,45 @@
-#!/bin/sh
-# **************************************************
-# Description  : Whether url list is heathy or not  
-# Build        : 2022-03-24 23:26
-# Author       : Kung
-# System       : CentOS 7.6 
-# Version      : 1.1
-#              :     
-# *************************************************
+#!/bin/bash
+# Author      : Kung
+# Build       : 2022-04-13 20:04
+# Version     : V1.0
+# Description : Monitor Web list           
+# System      : CentOS 7.6 
+			       
+export PS4='++ ${LINENO}'  
+export LANG=C
 export PATH=$PATH
-. /etc/init.d/functions
+[ -f /etc/init.d/functions ] && . /etc/init.d/functions
+Script_Path=/root/github
+[ ! -d ${Script_Path} ] && mkdir -p ${Script_Path}
+Log_Path=/root/tmp
+[ ! -d ${Log_Path} ] && mkdir -p ${Log_Path}
 
-url_count=0
-url_list=(
+Url_Count=0
+Url_List=(
 www.google.com
 www.kung.roc
 www.yahoo.com.tw
 www.sina.com.tw
-www.pchome.com.tw
 www.ithome.com.tw
 )
-function url_check(){
-	for((i=0;i<${#url_list[*]};i++  ))
+function Url_Check(){
+	for((i=0;i<${#Url_List[*]};i++))
 	do
-		wget -o /dev/null -T 3 --tries=1 --spider ${url_list[$i]}>/dev/null 2>&1
+		wget -o /dev/null -T 3 --tries=1 --spider ${Url_List[$i]}>/dev/null 2>&1
 		if [ $? -eq 0 ];then
-			action "${url_list[$i]}" /bin/true
+			action "${Url_List[$i]}" /bin/true
 		else
-			action "${url_list[$i]}" /bin/false
+			action "${Url_List[$i]}" /bin/false
 		fi		
 	done
-	((url_count++))
+	((Url_Count++))
 }
 
 function main(){
 	while true
 	do
-		url_check
-		echo "Please wait me five second!!! The count is $url_count time "
+		Url_Check
+		echo "Please wait me five second!!! The count is $Url_Count time "
 		sleep 5  
 	done
 }
