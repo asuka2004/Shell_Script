6140e9d54eab6c5f2f7d58cac10bb31363b15f12
Kung <asuka.2004@gmail.com>
2022-04-13 19:58:18 +0800

    2022-04-13-19:58

--- calculate.sh
+++ calculate.sh
@@ -1,31 +1,35 @@
-#!/bin/sh
-# **************************************************
-# Description  : Calculate sum 
-# Build        : 2022-03-24 23:40
-# Author       : Kung
-# System       : CentOS 7.6
-# Version      : 1.1
-#              :     
-# *************************************************
+#!/bin/bash
+# Author      : Kung
+# Build       : 2022-04-13 19:54
+# Version     : V1.0
+# Description : Sum Cal           
+# System      : CentOS 7.6 
+			       
+export PS4='++ ${LINENO}'  
+export LANG=C
 export PATH=$PATH
-. /etc/init.d/functions
+[ -f /etc/init.d/functions ] && . /etc/init.d/functions
+Script_Path=/root/github
+[ ! -d ${Script_Path} ] && mkdir -p ${Script_Path}
+Log_Path=/root/tmp
+[ ! -d ${Log_Path} ] && mkdir -p ${Log_Path}
 
 i=1
-sum=0
+sum_a=0
 while [ $i -lt 5 ]
 do
- 	((sum=sum+i))
+ 	((sum_a=sum_a+i))
  	((i++))
 done
-[ "$sum" -ne 0 ] && printf "Total sum is:$sum\n"
+[ "$sum_a" -ne 0 ] && printf "Total Sum is: $sum_a\n"
 
 
 echo " "
 
-sun=0
+sum_b=0
 for ((i=1;i<5;i++))
 do
-	((sun=sun+i))
+	((sum_b=sum_b+i))
 done
-[ "$sum" -ne 0 ] && printf "Total sum is:$sum\n"
+[ "$sum_b" -ne 0 ] && printf "Total Sum is: $sum_b\n"
  
