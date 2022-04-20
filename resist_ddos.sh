b5816b75f902ebd7b2da13501b2566416d55cf8d
Kung <asuka.2004@gmail.com>
2022-04-20 22:47:06 +0800

    2022-04-20-22:47

--- resist_ddos.sh
+++ resist_ddos.sh
@@ -1,28 +1,33 @@
-#!/bin/sh
-# **************************************************
-# Description  : resist ddos
-# Build        : 2022-03-25 23:05
-# Author       : Kung
-# System       : CentOS 7.6
-# Version      : 1.1
-#              :     
-# *************************************************
+#!/bin/bash
+# Author      : Kung
+# Build       : 2022-04-20 22:45
+# Version     : V1.0
+# Description : Resist DDOS           
+# System      : CentOS 7.6 
+			       
+export PS4='++ ${LINENO}'  
+export LANG=C
 export PATH=$PATH
-. /etc/init.d/functions
+[ -f /etc/init.d/functions ] && . /etc/init.d/functions
+Script_Path=/root/github
+[ ! -d ${Script_Path} ] && mkdir -p ${Script_Path}
+Log_Path=/root/tmp
+[ ! -d ${Log_Path} ] && mkdir -p ${Log_Path}
 
 file=$1
 while true
 do
- awk '{print $1}' $1 | grep -v "^$"|sort|uniq -c >/root/tmp.log
- exec</root/tmp.log
- while read line
-  do
-	ip=`echo $line|awk '{print $2}'`
-	count=`echo $line|awk '{print $1}'`
-	if [ $count -gt 30 ];then
-	firewall-cmd --add-rich-rule "rule family="ipv4" source address="$ip" port port="80" protocol="tcp"  reject"
-	echo "$line is dropped">>/root/droplist_$(date +F%).log
+	 awk '{print $1}' $1 | grep -v "^$"|sort|uniq -c >/root/tmp.log
+ 	exec</root/tmp.log
+ 	while read line
+  	do
+		ip=`echo $line|awk '{print $2}'`
+		count=`echo $line|awk '{print $1}'`
+		
+		if [ $count -gt 30 ];then
+		firewall-cmd --add-rich-rule "rule family="ipv4" source address="$ip" port port="80" protocol="tcp"  reject"
+		echo "$line is dropped">>/root/droplist_$(date +F%).log
 	fi
-  done
-  sleep 20
+  	done
+  	sleep 20
 done
