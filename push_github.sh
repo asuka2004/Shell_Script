b974b076ef8cd880bfc70c2c08af6aee88396366
Kung <asuka.2004@gmail.com>
2022-04-21 20:58:55 +0800

    2022-04-21-20:58

--- push_github.sh
+++ push_github.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-# **************************************************
-# Description  : push to github 
-# Build        : 2022-04-11 22:36
-# Author       : Kung
-# System       : CentOS 7.6
-# Version      : 1.1
-# ************************************************ 
+# Author      : Kung
+# Build       : 2022-04-21 20:58
+# Version     : V1.0
+# Description :            
+# System      : CentOS 7.6 
+			       
+export PS4='++ ${LINENO}'  
 export LANG=C
 export PATH=$PATH
-. /etc/init.d/functions
+[ -f /etc/init.d/functions ] && . /etc/init.d/functions
+Script_Path=/root/github
+[ ! -d ${Script_Path} ] && mkdir -p ${Script_Path}
+Log_Path=/root/tmp
+[ ! -d ${Log_Path} ] && mkdir -p ${Log_Path}
+
 script_path=/root/github
 log_path=/root/tmp
 	git add --all 
