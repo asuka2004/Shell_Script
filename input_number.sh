1369a3f3d5377125e2fe348dfa2a867b06a19879
Kung <asuka.2004@gmail.com>
2022-04-21 21:04:37 +0800

    2022-04-21-21:04

--- input_number.sh
+++ input_number.sh
@@ -1,14 +1,18 @@
-
 #!/bin/bash
-# Description  : Enter number and + - * %   
-# Build        : 2022-04-02 11:14
-# Author       : Kung
-# System       : CentOS 7.6
-# Version      : 1.1
-#              :     
-# *************************************************
+# Author      : Kung
+# Build       : 2022-04-21 21:04
+# Version     : V1.0
+# Description :            
+# System      : CentOS 7.6 
+			       
+export PS4='++ ${LINENO}'  
+export LANG=C
 export PATH=$PATH
-. /etc/init.d/functions
+[ -f /etc/init.d/functions ] && . /etc/init.d/functions
+Script_Path=/root/github
+[ ! -d ${Script_Path} ] && mkdir -p ${Script_Path}
+Log_Path=/root/tmp
+[ ! -d ${Log_Path} ] && mkdir -p ${Log_Path}
 
 print_usage(){
 	printf "Please enter an integer!!! Thks\n"
