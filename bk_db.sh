13c4a946618ed62dcf903bd0216133729b2736ec
Kung <asuka.2004@gmail.com>
2022-04-28 20:46:37 +0800

    2022-04-28-20:46

--- bk_db.sh
+++ bk_db.sh
@@ -23,10 +23,10 @@ MYDUMP="mysqldump -u$MYUSER -p$MYPASS -S $SOCKET"
 
 [ ! -d "$DBPATH" ] && mkdir $DBPATH
 
-for dbname in `$MYCMD -e "show databases"|sed '1,2d'|egrep -v "mysql|schema|performance_schema|sys|information_schema"` 
+for dbname in `$MYCMD -e "show databases"|sed '1d'|egrep -v "mysql|schema|performance_schema|sys|information_schema"` 
 do
- 	mkdir $DBPATH/${dbname}_$(date +%F) -p
-	for table in `$MYCMD -e "use $dbname;show tables from $dbname;"|sed '1,2d'`
+ 	mkdir -p $DBPATH/${dbname}_$(date +%F) 
+	for table in `$MYCMD -e "use $dbname;show tables;"|sed '1d'`
 	do
 	$MYDUMP $dbname $table |gzip >$DBPATH/${dbname}_$(date +%F)/${dbname}_${table}.sql.gz
 	done
