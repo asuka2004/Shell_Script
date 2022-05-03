c22e52cef43295a53ec225e23b743236259dd0e8
Kung <asuka.2004@gmail.com>
2022-05-03 23:06:22 +0800

    2022-05-03-23:06

--- bk_db.sh
+++ bk_db.sh
@@ -2,7 +2,7 @@
 # Author      : Kung
 # Build       : 2022-04-13 10:52
 # Version     : V1.0
-# Description : Batch&Backup table on MySql5.6.             
+# Description : Create&Backup&Restore database on MySql5.6             
 # System      : CentOS 7.6 
 			       
 export PS4='++ ${LINENO}'  
@@ -18,33 +18,43 @@ DBPATH=/root/tmp
 MYUSER=Kung
 MYPASS=P@ssw0rd
 SOCKET=/var/lib/mysql/mysql.sock
-MYCMD="mysql -u$MYUSER -p$MYPASS -S $SOCKET"
+MYCMD="mysql -u$MYUSER -p$MYPASS -S $SOCKET -h localhost"
 MYDUMP="mysqldump -u$MYUSER -p$MYPASS -S $SOCKET -h localhost --no-tablespaces --single-transaction"
 
 [ ! -d "$DBPATH" ] && mkdir $DBPATH
 
 Create_db(){
 	
-	for dbname in `seq -w 05`
+	for num in `seq -w 05` 
 	do
-		$MYCMD -e "create database student$dbname;"
+		$MYCMD -e "create database company$num;use company$num;create table dept(deptnu int primary key,dname VARCHAR(50),addr VARCHAR(50)) engine='InnoDB' DEFAULT CHARSET=utf8;create table employee(empno INT PRIMARY KEY ,ename VARCHAR(50) ,job VARCHAR(50),mgr INT ,hiredate DATE ,sal DECIMAL(4.2) ,deptnu INT ,foreign key (deptnu) references dept(deptnu)) engine='InnoDB' DEFAULT CHARSET=utf8;CREATE TABLE salgrade(grade INT PRIMARY KEY,lowsal INT ,highsal INT) engine='InnoDB' DEFAULT CHARSET=utf8;"
 	done
 }
+
 Backup_db(){
-	for dbname in `$MYCMD -e "show databases"|sed '1d'|egrep -v "mysql|schema|performance_schema|sys|information_schema"` 
+	for dbname in `$MYCMD -e "show databases;"|sed '1d'|egrep -v "mysql|schema|performance_schema|sys|information_schema"` 
 	do
  		mkdir -p $DBPATH/${dbname}_$(date +%F) 
-		for table in `$MYCMD -e "use $dbname;show tables;"|sed '1d'`
-		do
-			$MYDUMP $dbname $table |gzip >$DBPATH/${dbname}_$(date +%F)/${dbname}_${table}.sql.gz
-		done
+		$MYDUMP $dbname |gzip >$DBPATH/${dbname}_$(date +%F)/${dbname}.sql.gz
+	done
+}
+
+Restore_db(){
+	for dbname in `$MYCMD -e "show databases"|sed '1d'|egrep -v "mysql|schema|performance_schema|sys|information_schema"` 
+	do
+		gunzip ${DBPATH}/${dbname}_2022-05-03/${dbname}.sql.gz 
+		$MYCMD ${dbname}<${DBPATH}/${dbname}_$(date +%F)/${dbname}.sql	
 	done
 }
 
 Main(){
 	Create_db
+	echo "Wait 5 second....."
+	sleep 5
 	Backup_db
+	echo "Wait 5 second....."
+	sleep 5
+	Restore_db	
 }
 Main
-
 
