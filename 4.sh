23598e697add90ec9153ed474e78a002c384c0c7
Kung <asuka.2004@gmail.com>
2022-05-10 16:11:25 +0800

    a

---
+++ 4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author      : Tony
+# Build       : 2022-05-10 16:10
+# Version     : V1.0
+# Description :            
+# System      : CentOS 7.6 
+# ************************************************ 
+export PS4='++ ${LINENO}'  
+export LANG=C
+export PATH=$PATH
+[ -f /etc/init.d/functions ] && . /etc/init.d/functions
+Script_Path=/root/github
+Log_Path=/root/tmp
+
