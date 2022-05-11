678a94a0a36951818c797953f5390a54ecbd3421
asuka <asuka.2004@gmail.com>
2022-05-11 23:28:20 +0800

    2022-05-11-23:28

--- study_break.sh
+++ study_break.sh
@@ -1,14 +1,18 @@
-#!/bin/sh
-# **************************************************
-# Description  : study "break continue exit return" 
-# Build        : 2022-03-25 21:43
-# Author       : Kung
-# System       : CentOS 7.6
-# Version      : 1.1
-#              :     
-# *************************************************
+#!/bin/bash
+# Author      : Kung
+# Build       : 2022-05-11 23:27
+# Version     : V1.0
+# Description : Study break continue exit return           
+# System      : CentOS 7.6 
+			       
+export PS4='++ ${LINENO}'  
+export LANG=C
 export PATH=$PATH
-. /etc/init.d/functions
+[ -f /etc/init.d/functions ] && . /etc/init.d/functions
+Script_Path=/root/github
+[ ! -d ${Script_Path} ] && mkdir -p ${Script_Path}
+Log_Path=/root/tmp
+[ ! -d ${Log_Path} ] && mkdir -p ${Log_Path}
 
 if [ $# -ne 1 ]
  then
