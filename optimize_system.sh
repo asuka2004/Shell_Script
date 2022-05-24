83f2213c3de09f1e5b505956235b99382b551ac8
Kung <asuka.2004@gmail.com>
2022-05-24 17:54:09 +0800

    modify optimize file

--- optimize_system.sh
+++ optimize_system.sh
@@ -16,29 +16,17 @@ Log_Path=/root/tmp
 
 if [ "$UID" != "0" ]
  then
-	echo "Please run the script by root"	
+	echo "Please run the script by root"
 	exit 1
 fi
 
-
-function adduser(){
-	if [ `grep -w Kung /etc/passwd|wc -l` -lt 1 ]
-	 then
-		useradd Kung
-		echo P@ssw0rd| passwd --stdin Kung
-		cp /etc/sudoers /etc/sudoers.ori
-		echo "Kung ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
-		visudo -c &>/dev/null	
-	fi
+function set_timezone(){
+	cp /etc/locale.conf /etc/locale.conf.`date +"%Y-%m-%d"`
+	localectl set-locale LANG=C
+	cat  /etc/locale.conf
 }
 
-function charset(){
-	cp /etc/sysconfig/il8n /etc/sysconfig/il8n.ori
-	echo 'LANG=C' > /etc/sysconfig/il8n
-	source /etc/sysconfig/il8n
-}
-
-function time_sync(){
+function sync_time(){
 	cron=/var/spool/cron/root
 	if [ `grep -w "ntpdate" $cron|wc -l` -lt 1 ]
 	 then
@@ -48,14 +36,23 @@ function time_sync(){
 	fi
 }
 
-function open_file(){
+function opt_file(){
 	if [ `grep 65535 /etc/security/limits.conf|wc -l` -lt 1 ]
 	 then
 		echo '* - nofile 65535 '>>/etc/security/limits.conf
 	fi
+	ulimit -SHn 65535
 }
 
-function set_kernel(){
+function opt_profile(){
+	echo 'export TMOUT=300' >>/etc/profile
+	echo 'export HISTSIZE=5' >> /etc/profile
+	echo 'export HISTFILESIZE=5' >> /etc/profile
+	tail -3 /etc/profile
+	. /etc/profile
+}
+
+function opt_kernel(){
 	if [ `grep kernel_flag /etc/sysctl.conf|wc -l` -lt 1 ]
 	 then
 		cat >> /etc/sysctl.conf<<EOF
@@ -82,57 +79,38 @@ EOF
 		sysctl -p
 
 	fi
-function init_ssh(){
+}
+
+function opt_sshd(){
 	cp /etc/ssh/sshd_config /etc/ssh/sshd_config.`date +"%Y-%m-%d"`
-	sed -i 's%#PermitRootLogin yes%PermitRootLogin no%' /etc/ssh/sshd_config
-	sed -i 's%#PermitEmptyPasswords no%PermitEmptyPasswords no%' /etc/ssh/sshd_config
-	sed -i 's%#UseDNS yes%UseDNS no%' /etc/ssh/sshd_config
-	/etc/init.d/sshd reload &>/dev/null	
-}
-
-function disable_service(){
-	systemctl list-unit-files | grep enable| egrep -v "sshd.service|cron.service|sysstat|rsyslog|NetworkManager.service|irqbalance.service" |awk '{print "systemctl disable",$1}'
-}
-
-function timezone(){
-	cp /etc/locale.conf{,.ori}
-	localectl set-locale LANG=C
-	cat /etc/locale.conf
-}
-
-function correct_time(){
-	ntpdate time.stdtime.gov.tw
-	echo '#cron-id-001:time sync by Kung'>>/var/spool/cron/root
-	echo "*/5 * * * * /usr/sbin/ntpdate time.stdtime.gov.tw > /dev/null 2>&1" >>/var/spool/cron/root
-	crontab -l		
-}
-
-function command(){
-	echo 'export TMOUT=300' >>/etc/profile
-	echo 'export HISTSIZE=5' >> /etc/profile
-	echo 'export HISTFILESIZE=5' >> /etc/profile
-	tail -3 /etc/profile
-	. /etc/profile
-}
-
-function limit(){
-	echo '*		- 	nofile 		65535'>>/etc/security/limits.conf
-	tail -l /etc/security/limits.conf
-	ulimit -SHn 65535
-	ulimit -n
-}
-
-function ssh(){
-	cp /etc/ssh/sshd_config{,.ori}
 	sed -i -e "17s/.*/Port 55555/g" /etc/ssh/sshd_config
 	sed -i -e "19s/.*/ListenAddress 192.168.88.51/g" /etc/ssh/sshd_config
 	sed -i -e "38s/.*/PermitRootLogin no/g" /etc/ssh/sshd_config
 	sed -i -e "64s/.*/PermitEmptyPasswords no/g" /etc/ssh/sshd_config 
 	sed -i -e "79s/.*/GSSAPIAuthentication no/g" /etc/ssh/sshd_config
 	sed -i -e "115s/.*/UseDNS no/g" /etc/ssh/sshd_config
+	/etc/init.d/sshd reload &>/dev/null
 }
 
-function file(){
+
+function add_user(){
+	if [ `grep -w Kung /etc/passwd|wc -l` -lt 1 ]
+	 then
+		useradd Kung
+		echo P@ssw0rd| passwd --stdin Kung
+		cp /etc/sudoers /etc/sudoers.ori
+		echo "Kung ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
+		visudo -c &>/dev/null	
+	fi
+}
+
+
+function disable_service(){
+	systemctl list-unit-files | grep enable| egrep -v "sshd.service|cron.service|sysstat|rsyslog|NetworkManager.service|irqbalance.service" |awk '{print "systemctl disable",$1}'
+}
+
+
+function lock_file(){
 	chattr +i /etc/passwd /etc/shadow /etc/group
 	chattr +i /etc/inittab /etc/fstab /etc/sudoers
 	lsattr /etc/passwd /etc/shadow /etc/group /etc/inittab /etc/fstab /etc/sudoers
@@ -140,27 +118,16 @@ function file(){
 	cp /usr/bin/any /usr/bin/chattr
 }
 
-function issue(){
+function clear_issue(){
 	cat /dev/null>/etc/issue
 	cat /dev/null>/etc/issue.net
 }
 
-function ping(){
+function ban_ping(){
 	echo "net.ipv4.icmp_echo_ignore_all=1">> /etc/sysctl.conf
 }
 
-function alias(){
-	echo "alias grep='grep --color=auto'">>/etc/profile
-	source /etc/profile	
-}
-
-
 main(){
-	init_ssh
-	set_kernel
-	ope_file
-	time_sync
-	charset
-	adduser
+
 }
 main
