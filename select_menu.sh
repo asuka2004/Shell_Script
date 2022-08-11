d7afc5c73fd80ec702f88691a6b85e3267bc393c
asuka <asuka.2004@gmail.com>
2022-08-11 19:26:54 +0800

    modify system

--- select_menu.sh
+++ select_menu.sh
@@ -3,13 +3,13 @@
 # Build       : 2022-04-21 21:06
 # Version     : V1.0
 # Description :            
-# System      : CentOS 7.6 
+# System      : CentOS 7.9 
 			       
 export PS4='++ ${LINENO}'  
 export LANG=C
 export PATH=$PATH
 [ -f /etc/init.d/functions ] && . /etc/init.d/functions
-Script_Path=/root/github
+Script_Path=/root/project/github
 [ ! -d ${Script_Path} ] && mkdir -p ${Script_Path}
 Log_Path=/root/tmp
 [ ! -d ${Log_Path} ] && mkdir -p ${Log_Path}
