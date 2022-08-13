dd4b9cd34775dd61017c07889c0c020dfc1cddae
asuka <asuka.2004@gmail.com>
2022-08-13 19:56:53 +0800

    modify file

---
+++ create_sqldata.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Author      : Kung
+# Build       : 2022-05-05 23:22
+# Version     : V1.0
+# Description : Random lot of information to insert mysql5.6           
+# System      : CentOS 7.6 
+			       
+export PS4='++ ${LINENO}'  
+export LANG=C
+export PATH=$PATH
+[ -f /etc/init.d/functions ] && . /etc/init.d/functions
+Script_Path=/root/project/github
+[ ! -d ${Script_Path} ] && mkdir -p ${Script_Path}
+Log_Path=/root/tmp
+[ ! -d ${Log_Path} ] && mkdir -p ${Log_Path}
+
+echo "Please input server number:"
+read server
+echo "Please input sql number:"
+read number
+# char=`head /dev/urandom | tr -dc 0-9 | head -c 11`
+for (( i=0;i<$number;i++ ))
+do
+pass=`head /dev/urandom | tr -dc a-z | head -c 8`
+let server=server+1
+echo "insert into test(id,username,servnumber,password,createtime)
+values('$i','user${i}','${serber}','$pass',now());" >>/root/tmp/sql.txt
+done
--- cut_nginxlog.sh
+++ cut_nginxlog.sh
@@ -2,7 +2,7 @@
 # Author      : Kung
 # Build       : 2022-05-28 19:46
 # Version     : V1.0
-# Description : Cut Nginx log            
+# Description : Cut Nginx log every day            
 # System      : CentOS 7.9 
 			       
 export PS4='++ ${LINENO}'  
@@ -14,14 +14,14 @@ Script_Path=/root/project/github
 Log_Path=/root/tmp
 [ ! -d ${Log_Path} ] && mkdir -p ${Log_Path}
 
-Dateformat=`date +%Y%m%d -d -1day`
+
+DataFormat=`date +%Y%m%d -d -1day`
 Basedir="/usr/local/nginx"
 Nginxlog="$Basedir/logs"
-Logname="access_www"
 
 [ -d $Nginxlog ] && cd $Nginxlog || exit 1
-[ -f ${Logname}.log ] || exit 1
-mv ${Logname}.log ${DateFormat}_${Logname}.log
+[ -f access.log ] || exit 1
+mv access.log access_${DataFormat}.log
 $Basedir/sbin/nginx -s reload
 
 
